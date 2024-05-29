c66c518b51b522f8821b01c11599b23e6271a117
Kacper Kołodziej <kacper@kolodziej.it>
2024-05-29 15:49:39 +0200

    wip: add RMT struct with basic init implementation

--- kernel/src/hil/rmt.rs
+++ kernel/src/hil/rmt.rs
@@ -5,9 +5,24 @@
 // Author: Kacper Kołodziej <kacper@sfinae.dev>
 
 use crate::ErrorCode;
+use crate::hil::gpio;
 
 #[derive(Copy, Clone, Debug)]
 pub enum DataOrder {
     MSBFirst,
     LSBFirst,
+}
+
+pub struct RMT<'a, P: gpio::Pin> {
+    pub pin: &'a P,
+}
+
+impl<'a, P: gpio::Pin> RMT<'a, P> {
+    fn new(p: &'a P) -> Self {
+        Self { pin: p }
+    }
+
+    fn init(&self) {
+        self.pin.make_output();
+    }
 }
